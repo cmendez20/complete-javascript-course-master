dd0b4feee00e6dfd81a378a22593856c400c45c4
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-11-27 14:51:02 -0600

    First commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = 'United States of America';
+let continent = 'North America';
+let population = '3,000,000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = 'Jonas';
+let first = 'jonas';
+let firstNamePerson = 'chris';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
