a4c00810fc3cc2b0ac6e6a4c4a4da7669a7111b4
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-11-28 23:41:51 -0600

    Modified starer final for 01-fundamentals-part-1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,16 @@
-let js = 'amazing';
-// if (js === 'amazing') alert('JavaScript is FUN!');
+// let js = 'amazing';
+// // if (js === 'amazing') alert('JavaScript is FUN!');
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = 'Jonas';
-let first = 'jonas';
-let firstNamePerson = 'chris';
+// let firstName = 'Jonas';
+// let first = 'jonas';
+// let firstNamePerson = 'chris';
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
