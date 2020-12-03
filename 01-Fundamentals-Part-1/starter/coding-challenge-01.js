fcfad8868f607ffe82b662125b009fc5ace617f6
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-12-02 19:10:52 -0600

    Completed coding challenge 02

--- 01-Fundamentals-Part-1/starter/coding-challenge-01.js
+++ 01-Fundamentals-Part-1/starter/coding-challenge-01.js
@@ -21,10 +21,21 @@ m tall.
 
 */
 
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const markBMI = markMass / (markHeight * markHeight);
+// const johnBMI = johnMass / (johnHeight * johnHeight);
+
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
 
 const markBMI = markMass / (markHeight * markHeight);
 const johnBMI = johnMass / (johnHeight * johnHeight);
@@ -32,13 +43,10 @@ const johnBMI = johnMass / (johnHeight * johnHeight);
 const markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
 
-// const markMass = 95;
-// const markHeight = 1.88;
-// const johnMass = 85;
-// const johnHeight = 1.76;
+//////////////////// CODING CHALLENGE 02
 
-// const markBMI = markMass / (markHeight * markHeight);
-// const johnBMI = johnMass / (johnHeight * johnHeight);
-
-// const markHigherBMI = markBMI > johnBMI;
-// console.log(markHigherBMI);
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
