084feada178cf8a4942aa0bcf1fb353b20a8273f
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-12-03 14:31:08 -0600

    Added section about type coercion & conversion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -11,18 +11,41 @@
 // console.log(firstName);
 // console.log(firstName);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 2);
+// console.log(ageJonas * 2, ageJonas / 2);
 
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + ' ' + lastName);
-// console.log(`${firstName} ${lastName}`);
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+// // console.log(`${firstName} ${lastName}`);
 
-let x = 10 + 5;
-x += 10;
-console.log(x);
+// let x = 10 + 5;
+// x += 10;
+// console.log(x);
+
+////////////////////////////////////
+//  Type Conversion
+///////////////////////////////////
+const inputYear = '1991';
+console.log(Number(inputYear), (inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Chris'));
+console.log(typeof NaN);
+// NaN is a number, but it is an invalid one.
+
+console.log(String(23), 23);
+
+////////////////////////////////////
+//  Type Coercion
+///////////////////////////////////
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
