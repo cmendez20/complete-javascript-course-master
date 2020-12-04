3b3d9d700f7cfbfe0695895e465754668fa8ac66
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-12-03 22:18:56 -0600

    Added section about falsy and truthy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,6 +30,7 @@
 ////////////////////////////////////
 //  Type Conversion
 ///////////////////////////////////
+/* 
 const inputYear = '1991';
 console.log(Number(inputYear), (inputYear));
 console.log(Number(inputYear) + 18);
@@ -49,3 +50,23 @@ console.log('23' - '10' - 3);
 let n = '1' + 1;
 n = n - 1;
 console.log(n);
+
+*/
+
+////////////////////////////////////
+//  5 Falsy Values: 0, "", undefined, null, NaN
+///////////////////////////////////
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if(height) {
+  console.log('Yay! Height is defined!');
+} else {
+  console.log('Height is UNDEFINED');
+}
