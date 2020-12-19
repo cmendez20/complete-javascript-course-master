81bf3f9b2a472af47603ff69c271172cb19342b6
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-12-19 14:10:50 -0600

    the for loop in javascript

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -313,3 +313,17 @@
 
 // // console.log(jonas['calcAge'](1991));
 // console.log(jonas.getSummary());
+
+
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+console.log('Lifting weights repeatition 1');
+console.log('Lifting weights repeatition 2');
+console.log('Lifting weights repeatition 3');
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
