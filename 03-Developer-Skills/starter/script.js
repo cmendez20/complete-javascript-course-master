f47cfca5fb77aa349913adba759b590abf91cebe
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-12-24 02:46:07 -0600

    Configured prettier

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const statement = 'This is a test!';
+
+console.log('Test staement');
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(x);
