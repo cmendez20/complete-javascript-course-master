e7c1e42042da8c228022462ff6ae3cedcf086114
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-12-31 00:21:39 -0600

    Started intro on DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+// document query selector grabs the element with the message class.
+// Then, the dot operator grabs the text content property on that element
+console.log(document.querySelector('.message').textContent);
