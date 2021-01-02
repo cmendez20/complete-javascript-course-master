3127154556ee0b1320db9e94fcf5350c71c87418
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-01-02 01:05:20 -0600

    Setting up guessing game with DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,4 +2,25 @@
 
 // document query selector grabs the element with the message class.
 // Then, the dot operator grabs the text content property on that element
+
+/*
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // The first sceanario is to assume that there is no input. Then, respond accordingly.
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
