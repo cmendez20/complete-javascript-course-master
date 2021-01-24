f0a111110f292ff6ddad2726f8284ede280eddab
Christopher Mendez <cmendez20@utexas.edu>
2021-01-24 15:13:29 -0600

    Implemented reset game function

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,14 +14,28 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// declaring a variable is not the same as ASSIGNING them a variable;
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -79,3 +93,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// we do not call the init function here with (),
+// it is JavaScript's job to call the function when we click on the New button.
+btnNew.addEventListener('click', init);
