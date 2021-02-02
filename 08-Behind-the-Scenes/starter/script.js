efa8c04720f11c45cd6f88f9d03cbccd83d17f19
Christopher Mendez <cmendez20@utexas.edu>
2021-02-02 12:24:35 -0600

    Worked with scoping examples

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you are a millenial, ${firstName}.`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    add(2, 3);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Chris';
+calcAge(1991);
