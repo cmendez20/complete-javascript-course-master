53286e744d96af6443221ba952b36f2dde047eaf
Christopher Mendez <cmendez20@utexas.edu>
2021-02-27 00:43:54 -0600

    Using optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,25 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+////////////////////////////////
+// Enhanced Object Literals
+const weekdays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -36,24 +55,37 @@ const restaurant = {
   },
 };
 
-////////////////////////////////
-// Enhanced Object Literals
-const weekdays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+// if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+// With optional chaining
+// Only if the property before the question mark exist (not null or not undefined), then the proceeding property will be read.
+// If it does not exist, then it returns undefined
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// REAL Example
+const days = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+// const users = [
+//   {
+//     name: 'Jonas',
+//     email: 'Jonas@email.com',
+//   },
+// ];
+
+const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
 
 ////////////////////////////////
 // Looping Arrays using the for-of loop
