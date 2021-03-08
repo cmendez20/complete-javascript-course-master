6223f45fd35d57c80c24ea74ae9dde8aaf1a1879
Christopher Mendez <cmendez20@utexas.edu>
2021-03-07 22:13:18 -0600

    Working with map iterations

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -55,39 +55,72 @@ const restaurant = {
   },
 };
 
-//////////////////////////// MAPS: THE FUNDAMENTALS
-// A map is a data structure that we can use to map data to keys
+/////////////////// MAPS: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct Answer!'],
+  [false, 'Wrong answer :('],
+]);
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Florence, Italy');
-rest.set(2, 'Lisbon, Portugal');
+console.log(question);
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are closed :(');
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// Quiz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
 
-console.log(rest.has('categories'));
-rest.delete(2);
+console.log(question.get(question.get('correct') === answer));
 
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.size);
-// rest.clear();
+// Convert map to array
+console.log(...question);
 
-console.log(rest.get(arr));
+// //////////////////////////// MAPS: THE FUNDAMENTALS
+// // A map is a data structure that we can use to map data to keys
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Florence, Italy');
+// rest.set(2, 'Lisbon, Portugal');
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+// // rest.clear();
+
+// console.log(rest.get(arr));
 
 ////////////////// SETS
 
