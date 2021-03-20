4846738febe554404b9a144dceb88528846d66b9
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-03-20 00:43:53 -0500

    Working with default parameters with Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 Way to Set Default Parameters
+  //numPassengers = numPassengers || 1;
+  //price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// setting a parameter as undefined is the same thing as not setting the parameter, thus, falling back to the default parameter
+createBooking('LH123', undefined, 1000);
