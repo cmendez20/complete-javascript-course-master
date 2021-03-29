2970d5d727f445b34b83f60c35334e6a254d6d4b
Christopher Mendez <cmendez20@utexas.edu>
2021-03-29 16:07:59 -0500

    Working with callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -28,37 +28,68 @@
 // // setting a parameter as undefined is the same thing as not setting the parameter, thus, falling back to the default parameter
 // createBooking('LH123', undefined, 1000);
 
-const flight = 'LH234';
-const chris = {
-  name: 'Chris Mendez',
-  passport: 23739479284,
-};
+// const flight = 'LH234';
+// const chris = {
+//   name: 'Chris Mendez',
+//   passport: 23739479284,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 23739479284) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
+//   if (passenger.passport === 23739479284) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
 
-checkIn(flight, chris);
+// checkIn(flight, chris);
 
-console.log(flight);
-console.log(chris);
+// console.log(flight);
+// console.log(chris);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
-};
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
 
-newPassport(chris);
-checkIn(flight, chris);
+// newPassport(chris);
+// checkIn(flight, chris);
 
 // Dealing with functions:
 // 1) Passing by value
 // 2) Passing by reference (JavaScript DOES NOT have passing by reference)
 // Only passsing by value
 // The reference of objects is a value, an address. Not a reference!
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('🙌');
+};
+
+// addEventListener is the higher-order function,
+// high5 is the callback function
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
