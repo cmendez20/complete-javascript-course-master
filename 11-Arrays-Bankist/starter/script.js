083d478c994c764d1220bf93cb815ed055e8e30d
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-04-27 14:10:07 -0500

    Using forEach with maps and sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -117,6 +117,8 @@ console.log(letters.join(' - '));
 
 */
 
+/*
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 for (const movement of movements) {
@@ -139,3 +141,23 @@ movements.forEach(function (movement, index, array) {
 
 // forEach is a higher order funciton, takes in a callback function
 // first parameter is current el in array, second parameter is current index of array
+
+*/
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, _value, map) {
+  console.log(`${value}: ${_value}`);
+});
