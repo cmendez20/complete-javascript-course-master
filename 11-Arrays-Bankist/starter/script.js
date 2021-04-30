d849e2efa9bda22fb6999c69649b898f6e10afcb
Chris Mendez <cmendez20@utexas.edu>
2021-04-30 13:36:57 -0500

    Using the map method with arrow functions

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -186,3 +186,21 @@ currenciesUnique.forEach(function (value, _value, map) {
 });
 
 */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementDescriptions = movements.map(
+  (mov, index) =>
+    `Movement ${index + 1}: You ${
+      mov > 0 ? `deposited` : `withdrew`
+    } ${Math.abs(mov)}`
+);
+
+console.log(movementDescriptions);
