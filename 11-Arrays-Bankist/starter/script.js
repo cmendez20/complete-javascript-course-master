c0515a06b8a3522a75106a8e78474a5f602e6cda
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-05-02 14:58:12 -0500

    Working through computing usernames with functional programming

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -82,8 +82,18 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
-const user = 'Steven Thomas Williams'; // stw
-const username = user.toLowerCase().;
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
