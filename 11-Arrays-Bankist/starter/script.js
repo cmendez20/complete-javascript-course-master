7462643321e76b4ac963d60438e7910f10c248b5
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-05-02 20:16:23 -0500

    Working with the filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -200,6 +200,8 @@ currenciesUnique.forEach(function (value, _value, map) {
 
 */
 
+console.log('-----------LECTURE---------------');
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1;
@@ -217,3 +219,14 @@ const movementDescriptions = movements.map(
 );
 
 console.log(movementDescriptions);
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+const deposits = movements.filter(mov => mov > 0);
+
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
