e9d6b89d69bdef6261d50a95e6e52536266a3b43
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-05-13 01:03:05 -0500

    More ways of creating and filling arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -447,12 +447,14 @@ console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 */
 
+/*
 // flat method only goes one level deep
 const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(arr.flat());
 
 const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
 console.log(arrDeep.flat(2));
+*/
 
 /*
 const accountMovements = accounts.map(acc => acc.movements);
@@ -480,6 +482,8 @@ const overalBalance2 = accounts
 console.log(overalBalance2);
 */
 
+// SORTING ARRAYS
+/*
 // Strings - the sort method mutates the original array
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort());
@@ -505,3 +509,32 @@ movements.sort((a, b) => a - b);
 movements.sort((a, b) => b - a);
 
 console.log(movements);
+*/
+
+/////// MORE WAYS OF CREATING AND FILLING ARRAYS
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(7);
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
